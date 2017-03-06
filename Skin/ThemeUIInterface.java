3eaf4c9fb8db789b46f81cfd08e54fd91cb965cb
Foreshadowing <905234358@qq.com>
2017-03-06 11:11:07 +0800

    test。。

--- Skin/ThemeUIInterface.java
+++ Skin/ThemeUIInterface.java
@@ -4,11 +4,13 @@ import android.content.res.Resources;
 import android.view.View;
 
 /**
- *
+ *  自定义控件 的接口 用于切换皮肤
  */
 public interface ThemeUIInterface {
 
     public View getView();
 
     public void setTheme(Resources.Theme themeId);
+
+
 }
